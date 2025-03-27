8563be1f46645ce869a9df3de8b534682ab506a8
ALSENEK <aleksandar.senek@thermofisher.com>
2025-03-27 12:56:20 +0100

    First listing test

--- flask_app.py
+++ flask_app.py
@@ -187,8 +187,8 @@ def etl_db():
      pages = booli_find_number_of_pages_data(url_booli_uppsala_kommun)
      links = booli_scrape_links(url_booli_uppsala_kommun, pages)
      listings = booli_scrape_objects(links)
-     for listing in listings:
-         listing.store_in_db()
+     #for listing in listings:
+     listings[0].store_in_db()
      print("ETL process finished")
 
 @app.route('/')
